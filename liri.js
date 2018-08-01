bba7d280b9e4eec07d4efd7895f631e8c902ec76
Will <williamjamesthompson@gmail.com>
2018-08-01 16:32:03 -0500

    Worked out some issues and got closer to finished

--- liri.js
+++ liri.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 var inquirer = require("inquirer");
 var request = require("request");
 var Spotify = require('node-spotify-api');
-
+var fs = require("fs");
 
 var keys = require("./keys.js");
 var Twitter = require("twitter");
@@ -17,12 +17,12 @@ var client = new Twitter(keys.twitter);
 
 for (var i = 3; i < nodeArgs.length; i++) {
     if (i > 3 && i < nodeArgs.length) {
-      search = search + "+" + nodeArgs[i];
+        search = search + "+" + nodeArgs[i];
     }
     else {
-      search += nodeArgs[i];
+        search += nodeArgs[i];
     }
-  }
+}
 
 
 
@@ -41,6 +41,9 @@ if (command === "my-tweets") {
 
 // Spotify Commands
 if (command === "spotify-this-song") {
+        if (process.argv[3] === undefined) {
+            search = "The Sign Ace of Base";
+        }
 
     spotify.search({ type: "track", query: search, limit: 20 }, function (err, qs) {
         if (err) {
@@ -57,10 +60,15 @@ if (command === "spotify-this-song") {
 
 
 
-
-var queryUrl = "http://www.omdbapi.com/?t=" + search + "&y=&plot=short&apikey=trilogy";
 // Movie Commands
+
 if (command === "movie-this") {
+    if (process.argv[3] === undefined) {
+        search = "Mr. Nobody";
+    }
+    
+    var queryUrl = "http://www.omdbapi.com/?t=" + search + "&y=&plot=short&apikey=trilogy";
+
     request(queryUrl, function (error, response, body) {
 
         // If the request is successful
@@ -78,5 +86,33 @@ if (command === "movie-this") {
             console.log("Actors: " + JSON.parse(body).Actors);
         }
     });
+}
 
+
+// Movie Commands
+if (command === "do-what-it-says") {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+
+        // If the code experiences any errors it will log the error to the console.
+        if (error) {
+            return console.log(error);
+        }
+
+        // We will then print the contents of data
+        var dataArr = data.split(",");
+
+        // We will then re-display the content as an array for later use.
+        spotify.search({ type: "track", query: dataArr[1], limit: 20 }, function (err, qs) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+
+            console.log("Track Name: " + qs.tracks.items[0].name);
+            console.log("Artist(s): " + qs.tracks.items[0].artists[0].name);
+            console.log("Preview Link: " + qs.tracks.items[0].preview_url);
+            console.log("Album: " + qs.tracks.items[0].album.name);
+        });
+
+
+    });
 } 
